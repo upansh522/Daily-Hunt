e5b86f84158d91b7fc48dd9cd4e8b696f55c8a65
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2024-07-05 11:32:35 +0530

    New API Updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "505a64c11d7c416b8023c53d42b7455e";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
